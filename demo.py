68196765172482a11c20bf1346740e54a334b96a
Tats777 <54814305+Tats777@users.noreply.github.com>
2020-10-30 12:04:08 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -19,12 +19,12 @@ tf.reset_default_graph()
 model = Latex("model", mean_train, std_train, plotting=False)
 
 def predict_single(img_file):
-    'function to take image and return prediction'
-    try:
-    	formula = io.imread(img_file)
-	    formula = cv2.cvtColor(formula, cv2.COLOR_BGR2GRAY)
-    	latex = model.predict(formula)
-    	return {'equation': latex['equation']}
+	'function to take image and return prediction'
+	try:
+		formula = io.imread(img_file)
+		formula = cv2.cvtColor(formula, cv2.COLOR_BGR2GRAY)
+		latex = model.predict(formula)
+		return {'equation': latex['equation']}
 	except:
 		formula = io.imread(img_file)
     	latex = model.predict(formula)
